454a78ada241263e9a65d694b8f2e7a8e9cfa45e
bhandhariya <sainiraja59@gmail.com>
2019-07-31 00:22:35 +0530

    register started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 
@@ -42,7 +42,7 @@ import { RegisterComponent } from './register/register.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
+    HttpClientModule,
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { HttpClient } from '@angular/common/http';
+import * as Swal from "sweetalert2";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +12,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AngularFireAuth,private http:HttpClient,private router:Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +24,31 @@ export class RegisterComponent implements OnInit {
   })
   registerFormSubmit(r){
     console.log(r)
+    this.auth.auth.createUserWithEmailAndPassword(r.email,r.password).then(()=>{
+      this.auth.auth.currentUser.updateProfile({
+        displayName:r.name
+      }).then(()=>{
+        var user=this.auth.auth.currentUser;
+        var obj={
+          name:user.displayName,
+          email:user.email,
+          providerId:user.providerId,
+          uid:user.uid,
+          emailVerified:user.emailVerified
+         }
+         this.http.post('/api/users/register',obj).subscribe(this.cb)
+      })
+    }).catch(error=>{
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      Swal.default.fire("Oops!", errorMessage, "error");
+    })
+  }
+cb=(dt)=>{
+  if(dt.uid){
+    Swal.default.fire("Great", "User Registered Successfully", "success");
+    this.router.navigate(['login'])
   }
 
+}
 }
