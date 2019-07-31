40ac8962325324d9fe53ed23809bdda4643e0e1a
bhandhariya <sainiraja59@gmail.com>
2019-07-31 11:54:12 +0530

    login function Done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFireDatabase } from "@angular/fire/database";
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,7 +12,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor( private router:Router,private auth:AngularFireAuth,private db:AngularFireDatabase) { }
+  constructor( private router:Router,private auth:AngularFireAuth,private http:HttpClient) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,31 @@ export class LoginComponent implements OnInit {
   })
   loginFormSubmit(r){
     console.log(r)
+  var userLogin=this.auth.auth.signInWithEmailAndPassword(r.email,r.password);
+  var user=this.auth.auth.currentUser;
+  userLogin.then(()=>{
+    if(user){
+      var obj={
+        uid:user.uid,
+        email:user.email
+      }
+      this.http.post('/api/users/login',obj).subscribe(this.cb)
+    }
+  })
   }
-
+  cb=(dt)=>{
+    console.log(dt)
+        console.log(dt.user._id)
+    sessionStorage.setItem('Token',dt.token);
+    sessionStorage.setItem('UID',dt.user.uid);
+    sessionStorage.setItem('MID',dt.user._id)
+    Swal.fire({
+      position: 'top-end',
+      type: 'success',
+      title: 'User login Successfully ',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    this.router.navigateByUrl('dashboard')
+  }
 }
