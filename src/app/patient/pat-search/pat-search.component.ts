42256ab6a666f981f17d1bd46c64c4d4079fdb25
bhandhariya <sainiraja59@gmail.com>
2019-08-02 17:33:03 +0530

    patient search comp created

---
+++ src/app/patient/pat-search/pat-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pat-search',
+  templateUrl: './pat-search.component.html',
+  styleUrls: ['./pat-search.component.scss']
+})
+export class PatSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
